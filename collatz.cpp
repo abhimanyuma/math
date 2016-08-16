022452cb900a7f5744972160d2f784f220b4b0ab
Abhimanyu Mongandh Ambalath <abhimanyu.m.a@gmail.com>
2016-08-16 08:00:24 +0530

    Improve Collatz to do 100 million in 4 seconds

--- collatz.cpp
+++ collatz.cpp
@@ -1,29 +1,50 @@
-//#include <cassert>
+
 #include <cstring>
-#include <map>
-//using namespace std;
+#include <cstdio>
+
 int main() {
-  //printf("%d %d\n",sizeof(int), sizeof(long long int));
-  int x = 10000000;
-  long long int j = 0;
-  int count = 0;
-  std::map<long long int, int> val_map;
-  //memset(array, 0, sizeof(int) * x);
-  for(int i=1;i<=x;i++){
-    count = 0;
-    j = i;
-    while (j>1) {
-      if (val_map[j]) {
-        count = count + val_map[j];
+
+  int n = 100000000;             //n is the total number for which collatz
+                                 //sequence length is computed
+  int mem_length = n;            //mem_length is the number till which it is memoized
+  long long int current_num = 0;
+  int seq_length = 0;
+
+  int *val_map = new int[mem_length];
+  memset(val_map, 0, sizeof(int) * mem_length);
+
+  for(int i=1;i<=n; i++){
+
+    seq_length = 0;
+    current_num = i;
+
+    while (current_num > 1) {
+
+      //Yay!! We found a memoized value
+      if ((current_num < mem_length) && val_map[current_num]) {
+        seq_length = seq_length + val_map[current_num];
         break;
-      } else if ((((j >> 1) << 1) -j) == 0) {
-        j = j >> 1;
-        count++;
-      } else {
-        j = ((3 * j) + 1) >> 1;
-        count += 2;
+      }
+
+      //Check if number is even using j & 1 rather than j%2 because
+      //this is much faster
+      else if ((current_num & 1) == 0) {
+        current_num = current_num >> 1;   //Don't divide by 2, shift right
+        seq_length++;
+      }
+
+      // If odd do two steps at once
+      else {
+        current_num = ((3 * current_num) + 1) >> 1; //Don't divide, shift right
+        seq_length += 2;
       }
     }
-    val_map[i] = count;
+
+    // If number within memoization limit memoize
+    if (i < mem_length)
+      val_map[i] = seq_length;
+
+    //For debug purposes
+    //printf("%d %d\n",i,seq_length);
   }
 }
